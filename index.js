f17abad86c1261b9ee5bae9e780b091e196cc1eb
DeathcallXd <om_s@mt.iitr.ac.in>
2022-03-30 09:59:14 +0530

    Covid 19 Dashboard

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.use('/api/google-login',googleLoginRoute);
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static('client/build'));
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`)))
